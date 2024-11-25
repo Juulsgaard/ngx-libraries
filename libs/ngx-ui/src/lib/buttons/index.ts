02c2523f94ba63cf32929b91cf159149295baa38
Frederik Sørensen <frederikjs1@hotmail.com>
2024-11-25 13:11:11 +0100

    Export base buttons

--- libs/ngx-ui/src/lib/buttons/index.ts
+++ libs/ngx-ui/src/lib/buttons/index.ts
@@ -1,5 +1,6 @@
 
 export * from './components/button/button.component';
 export * from './components/icon-button/icon-button.component';
+export * from './components/icon-button/icon-button.component';
 export * from './models/base-button.directive';
 export * from './models/base-icon-button.directive';

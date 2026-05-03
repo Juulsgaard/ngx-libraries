717fff8edb0ef0a57d6ee5a3467ce865e93793da
Frederik Sørensen <frederikjs1@hotmail.com>
2026-05-04 00:56:10 +0200

    More cleanup

--- libs/ngx-ui/src/models/index.ts
+++ libs/ngx-ui/src/models/index.ts
@@ -1,3 +1,3 @@
-
-export {UIScopeContext, UIScopeConfig, UIScope, provideUiScopeConfig, provideUiScope} from './ui-scope';
-export {NgxThemeColor} from './theme-colors';
+export {UIScopeContext, provideUiScopeConfig, provideUiScope} from './ui-scope';
+export type {UIScopeConfig, UIScope} from './ui-scope';
+export type {NgxThemeColor} from './theme-colors';

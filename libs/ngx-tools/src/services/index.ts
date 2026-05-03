481dfba279fb6c48cc1ca6cbe1e1ea44d12c0c14
Frederik Sørensen <frederikjs1@hotmail.com>
2026-05-04 00:06:56 +0200

    Clean up and set up pnpm workspaces

--- libs/ngx-tools/src/services/index.ts
+++ libs/ngx-tools/src/services/index.ts
@@ -1,6 +1,7 @@
-
 export * from './overlay/overlay.service';
 export * from './overlay/overlay.model';
 export * from './id-manager.service';
 export * from './route.service';
-export {IScrollContext, ScrollContext, provideScrollContext} from './scroll-context';
+
+export type {IScrollContext} from './scroll-context';
+export {ScrollContext, provideScrollContext} from './scroll-context';
